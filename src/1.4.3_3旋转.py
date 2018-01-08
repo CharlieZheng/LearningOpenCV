7c966a8457ba82f9572c6b39b2f50f15b4ad71b5
秋天冬天 <CharlieZheng@users.noreply.github.com>
2018-01-08 15:15:36 +0800

    Create 1.4.3_3旋转.py

---
+++ src/1.4.3_3旋转.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+img = cv2.imread('/home/hanrong/Desktop/opencv/build/Zheng/0.jpeg',0)
+rows,cols = img.shape
+M = cv2.getRotationMatrix2D((cols/2,rows/2),90,4) # 第三个参数表示缩放比例
+dst = cv2.warpAffine(img,M,(cols,rows))
+cv2.imshow('dst', dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
