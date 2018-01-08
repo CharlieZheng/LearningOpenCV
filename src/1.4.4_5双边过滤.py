321f2685cca15aa4ef224ec62f51f0411f96150d
秋天冬天 <CharlieZheng@users.noreply.github.com>
2018-01-08 17:11:00 +0800

    Create 1.4.4_5双边过滤.py

---
+++ src/1.4.4_5双边过滤.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+img = cv2.imread('/home/hanrong/Desktop/opencv/build/Zheng/0.jpeg')
+
+
+blur = cv2.bilateralFilter(img,9,75,75)
+img[0:100, 0:100] = blur[0:100, 0:100]
+
+
+#kernel = cv2.getGaussianKernel(img, img.rows, img.cols )
+kernel = np.ones((5,5),np.float32)/25
+dst = cv2.filter2D(img,-1,kernel)
+
+
+plt.subplot(131),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(132),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.subplot(133),plt.imshow(dst),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
